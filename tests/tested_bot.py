d42d0cba73d163b443711157ea6a6608a2ed1a4c
Jacob Savel'ev (Laptop) <g6h6m238929@gmail.com>
2024-06-04 13:03:41 +0300

    Version 0.1 tested

--- tests/tested_bot.py
+++ tests/tested_bot.py
@@ -4,20 +4,21 @@ from aiogram.types import Message
 from aiogram.filters.command import Command
 
 with open("tested_token.txt", "r", encoding="utf-8") as file:
-	bot = Bot(token=file.read())
+    bot = Bot(token=file.read())
 dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start_reg(message: Message):
-        await message.answer("Напишите сообщение которое будет отправлено в канал!")
+    await message.answer("Напишите сообщение которое будет отправлено в канал!")
         
-@dp.message(F.text.lower().startswith("канал"))
+@dp.message()
 async def echo_channel(message:Message):
-	await message.answer("Проверьте канал!")
-	await bot.send_message(chat_id=-1001527506224, text=message.text)
+    await message.answer("Проверьте канал!")
+    await bot.send_message(chat_id=-1001527506224, text=message.text)
 	
 async def main():
-	await dp.start_polling(bot)
+    print("Бот запущен")
+    await dp.start_polling(bot)
 	
 if __name__ == "__main__":
-	run(main())
+    run(main())
